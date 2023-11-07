d49319ee52f0a27db32307e37854cf404a7f9717
Nicole Park <nicolepark@bellsouth.net>
2023-11-06 19:15:42 -0500

    added puppies to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-import './App.css'
+import { puppyList } from "./data.js";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+    const [puppies, setPuppies] = useState(puppyList);
+    console.log(puppies);
 
-  return (
-    <> 
-    </>
-  )
+    return (
+        <div className="App">
+            {puppies.map((puppy) => {
+                return <p key={puppy.id}>{puppy.name}</p>;
+            })}
+        </div>
+    );
 }
-
-export default App
+export default App;
